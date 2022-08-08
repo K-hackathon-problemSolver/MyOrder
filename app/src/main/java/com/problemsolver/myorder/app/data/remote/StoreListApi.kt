1afc7927e3b88a0fc783051f5762932ed23a955c
uzun <hyjhyj0901@pusan.ac.kr>
2022-08-08 11:37:53 +0900

    feat: api 작성 & repository 인터페이스, 구현체 추가
    #8

--- app/src/main/java/com/problemsolver/myorder/app/data/remote/StoreListApi.kt
+++ app/src/main/java/com/problemsolver/myorder/app/data/remote/StoreListApi.kt
@@ -2,6 +2,7 @@ package com.problemsolver.myorder.app.data.remote
 
 import com.problemsolver.myorder.app.data.remote.dto.StoreDTO
 import com.problemsolver.myorder.app.data.remote.dto.StoreListBodyDTO
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.Headers
 import retrofit2.http.POST
@@ -10,6 +11,6 @@ interface StoreListApi {
 
 	@Headers("Content-Type: application/json")
 	@POST("/store/list")
-	suspend fun getStoreList(@Body storeListBody: StoreListBodyDTO): List<StoreDTO>
+	suspend fun getStoreList(@Body storeListBody: StoreListBodyDTO): Response<List<StoreDTO>>
 
 }
