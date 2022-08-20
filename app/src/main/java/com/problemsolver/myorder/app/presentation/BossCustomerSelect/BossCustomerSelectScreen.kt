fd69231787e885a6bbe2c6bafe40226cd988c45d
kimsungjin <kimsungjin0907@gmail.com>
2022-08-20 11:12:24 +0900

    feat : BossCustomerSelectScreen 구현

---
+++ app/src/main/java/com/problemsolver/myorder/app/presentation/BossCustomerSelect/BossCustomerSelectScreen.kt
@@ -0,0 +1,105 @@
+package com.problemsolver.myorder.app.presentation.BossCustomerSelect
+
+import android.media.Image
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.problemsolver.myorder.R
+
+@Composable
+fun BossCustomerSelectScreen(){
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(color = Color.White)) {
+        SelectHeader()
+        SelectBody()
+    }
+}
+
+@Composable
+fun ColumnScope.SelectHeader(){
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .padding(horizontal = 40.dp, vertical = 50.dp)
+        .weight(3f)) {
+        Text(text = "어떤 일로 My Order를",
+        fontSize = 25.sp)
+        Text(text = "찾아주셨나요?",
+            fontSize = 25.sp)
+    }
+}
+
+@Composable
+fun ColumnScope.SelectBody(){
+    Spacer(modifier = Modifier.height(100.dp))
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .padding(horizontal = 20.dp)
+        .weight(7f),
+        horizontalArrangement = Arrangement.SpaceAround) {
+        Column(modifier = Modifier,
+        verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "특별한 주문을")
+            Text(text = "드리고 싶어요!")
+            Spacer(modifier = Modifier.height(10.dp))
+            OutlinedButton(onClick = { /*TODO*/ },
+                modifier= Modifier.size(130.dp),  //avoid the oval shape
+                shape = CircleShape,
+                border= BorderStroke(1.dp, Color(0xff78C3FA)),
+                contentPadding = PaddingValues(0.dp),
+                colors = ButtonDefaults.outlinedButtonColors(contentColor =  Color.White, backgroundColor = Color(0xff78C3FA))
+            ) {
+                Column(modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally) {
+                    Image(painter = painterResource(id = R.drawable.bossicon), contentDescription = "BossIcon",Modifier.size(50.dp))
+                    Spacer(modifier = Modifier.height(10.dp))
+                    Text(text = "사장님")
+                }
+            }
+        }
+        Column(modifier = Modifier,
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "특별한 주문을")
+            Text(text = "하고 싶어요!")
+            Spacer(modifier = Modifier.height(10.dp))
+            OutlinedButton(onClick = { /*TODO*/ },
+                modifier= Modifier.size(130.dp),  //avoid the oval shape
+                shape = CircleShape,
+                border= BorderStroke(1.dp, Color(0xff78C3FA)),
+                contentPadding = PaddingValues(0.dp),
+                colors = ButtonDefaults.outlinedButtonColors(contentColor =  Color.White, backgroundColor = Color(0xff78C3FA))
+            ) {
+               Column(modifier = Modifier.fillMaxWidth(),
+                   verticalArrangement = Arrangement.Center,
+                   horizontalAlignment = Alignment.CenterHorizontally) {
+                   Image(painter = painterResource(id = R.drawable.customericon), contentDescription = "CustomerIcon",Modifier.size(50.dp))
+                   Spacer(modifier = Modifier.height(10.dp))
+                   Text(text = "손님")
+               }
+            }
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun prjkjk(){
+    BossCustomerSelectScreen()
+}
