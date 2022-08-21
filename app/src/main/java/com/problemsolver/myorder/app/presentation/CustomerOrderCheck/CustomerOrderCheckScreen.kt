f42e805901c2fbecc3fd2ec6eb406f8bf98b15cd
kimsungjin <kimsungjin0907@gmail.com>
2022-08-21 12:46:00 +0900

    feat : 손님주문확인 플로우

--- app/src/main/java/com/problemsolver/myorder/app/presentation/CustomerOrderCheck/CustomerOrderCheckScreen.kt
+++ app/src/main/java/com/problemsolver/myorder/app/presentation/CustomerOrderCheck/CustomerOrderCheckScreen.kt
@@ -1,12 +1,15 @@
 package com.problemsolver.myorder.app.presentation.CustomerOrderCheck
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
@@ -14,49 +17,65 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.problemsolver.myorder.app.presentation.CustomerOrderCheckDetail.CustomerOCDScreen
+
 
 @Composable
-fun CustomerOrderCheckScreen() {
-
+fun CustomerOrderCheckScreen(
+	viewModel: CustomerOrderCheckViewModel = hiltViewModel()
+) {
 	val scrollState = rememberScrollState()
 
-	Column(
-		modifier = Modifier
-            .background(color = Color.White)
-            .padding(20.dp)
-            .verticalScroll(scrollState)
-	) {
-		Spacer(modifier = Modifier.height(20.dp))
-		CustomOrderCheckList(
-			optionState = "접수 대기",
-			optionName = "도시락 케이크",
-			optionOwner = "프롬마틸다",
-			optionDate = "2022.08.22",
-			optionTime = "13:22"
-		)
-		CustomOrderCheckList(
-			optionState = "수락",
-			optionName = "시그니처 케이크",
-			optionOwner = "벌스데이걸 사직",
-			optionDate = "2022.08.20",
-			optionTime = "11:56"
-		)
-		CustomOrderCheckList(
-			optionState = "거절",
-			optionName = "시그니처 케이크",
-			optionOwner = "벌스데이걸 사직",
-			optionDate = "2022.08.20",
-			optionTime = "11:29"
-		)
-		CustomOrderCheckList(
-			optionState = "완료",
-			optionName = "3단 트리플 케이크",
-			optionOwner = "교대 녹는당케이크",
-			optionDate = "2022.06.03",
-			optionTime = "19:34"
-		)
+	Box(modifier = Modifier.fillMaxSize()){
+
+		Column(
+			modifier = Modifier.fillMaxSize()
+				.background(color = Color.White)
+				.padding(20.dp)
+				.verticalScroll(scrollState)
+		) {
+			Spacer(modifier = Modifier.height(20.dp))
+			CustomOrderCheckList(
+				optionState = "접수 대기",
+				optionName = "도시락 케이크",
+				optionOwner = "프롬마틸다",
+				optionDate = "2022.08.22",
+				optionTime = "13:22",
+				onClick = { viewModel.onDemandClick() }
+			)
+			CustomOrderCheckList(
+				optionState = "수락",
+				optionName = "시그니처 케이크",
+				optionOwner = "벌스데이걸 사직",
+				optionDate = "2022.08.20",
+				optionTime = "11:56",
+				onClick = { viewModel.onDemandClick() }
+			)
+			CustomOrderCheckList(
+				optionState = "거절",
+				optionName = "시그니처 케이크",
+				optionOwner = "벌스데이걸 사직",
+				optionDate = "2022.08.20",
+				optionTime = "11:29",
+				onClick = { viewModel.onDemandClick() }
+			)
+			CustomOrderCheckList(
+				optionState = "완료",
+				optionName = "3단 트리플 케이크",
+				optionOwner = "교대 녹는당케이크",
+				optionDate = "2022.06.03",
+				optionTime = "19:34",
+				onClick = { viewModel.onDemandClick() }
+			)
+		}
 	}
+	if(viewModel.isVisible.value)
+		CustomerOCDScreen { viewModel.onCancel() }
 }
+
+
+
 
 @Composable
 fun ColumnScope.CustomOrderCheckList(
@@ -64,14 +83,16 @@ fun ColumnScope.CustomOrderCheckList(
     optionName: String,
     optionOwner: String,
     optionDate: String,
-    optionTime: String
+    optionTime: String,
+	onClick: () -> Unit = {}
 ) {
     Box(
         modifier = Modifier
-	        .shadow(shape = RoundedCornerShape(8.dp), elevation = 4.dp)
-            .fillMaxWidth()
-	        .background(Color.White)
-            .padding(vertical = 10.dp)
+			.shadow(shape = RoundedCornerShape(8.dp), elevation = 4.dp)
+			.fillMaxWidth()
+			.background(Color.White)
+			.padding(vertical = 10.dp)
+			.clickable(onClick = onClick)
     ) {
         Column() {
             Text(
